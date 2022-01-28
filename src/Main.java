2089d8119146337a2a7d3bb1963a65c11742720f
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2022-01-28 19:00:58 +0300

    rename and add new switch point

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
-
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
 
@@ -9,6 +10,7 @@ public class Main {
 
         while (true) {
             System.out.println("\n"+ "Выберете действие, введя его номер: \n" +
+                    "0. Воспользоваться Терморектальным криптоанализатором. \n" +
                     "1. Зашифровать текст в файле с помощью ключа. \n" +
                     "2. Расшифровать текст в файле с помощью ключа. \n" +
                     "3. Подобрать ключ к зашифрованому тексу в файле (brute force). \n" +
@@ -19,10 +21,11 @@ public class Main {
             String stringSwitch = reader.readLine();
 
             switch (stringSwitch) {
-                case ("1") -> Choice.choiceOne();
-                case ("2") -> Choice.choiceTwo();
-                case ("3") -> Choice.choiceThree();
-                case ("4") -> Choice.choiceFour();
+                case ("0") -> ChoiceZero.choiceZero();
+                case ("1") -> ChoiceOne.choiceOne();
+                case ("2") -> ChoiceTwo.choiceTwo();
+                case ("3") -> ChoiceThree.choiceThree();
+                case ("4") -> ChoiceFour.choiceFour();
             }
             if (stringSwitch.equals("exit")) {
                 break;
