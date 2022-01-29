d9572a2744244cc2fd08807a19110a4f7ddc3dee
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2022-01-29 19:13:40 +0300

    add new method isValidText

--- src/ChoiceThree.java
+++ src/ChoiceThree.java
@@ -3,6 +3,7 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Random;
 import java.util.Scanner;
 
 public class ChoiceThree {
@@ -28,28 +29,44 @@ public class ChoiceThree {
                 String string = bufferedReader.readLine();
                 stringBuilder.append(string).append("\n");
             }
-            // подумать как собрать статистику для правельных подборов ключа
-
             for (int i = 0; i <= caesarCipher.maxSize; i++) {
                 String deEncrypt = caesarCipher.deEncrypt(stringBuilder.toString(), i);
-                if (deEncrypt.contains(". ") || deEncrypt.contains(", ") || deEncrypt.contains("! ")
-                        || deEncrypt.contains("/? ")) {
-                    System.out.println(deEncrypt);
-                    System.out.println("Похоже на то что нужно ? Y/N");
-                    String nextLine = scanner.nextLine();
-                    if (nextLine.equals("Y") || nextLine.equals("y")) {
-                        key = i;
-                        break;
-                    } else if (nextLine.equals("N") || nextLine.equals("n")){
-                        i++;
-                    }
+                if (isValidText(deEncrypt)) {
+                    key = i;
+                    bufferedWriter.write(deEncrypt);
+                    break;
                 }
-                System.out.println(deEncrypt);
             }
-            String deEncryptString = caesarCipher.deEncrypt(stringBuilder.toString(), key);
-            bufferedWriter.write(deEncryptString + "\n");
         }
         System.out.println("Содержимое файла расшифровано методом перебора ключей. Ключ расшифровки K = " + key );
     }
 
+    private static boolean isValidText(String text) {
+
+        boolean flag = false;
+
+        String[] strings = text.split(" ");
+        for (String string : strings) {
+            if (string.length() > 24) {
+                return false;
+            }
+        }
+        if (text.contains(". ") || text.contains(", ") || text.contains("! ") || text.contains("/? ")) {
+            flag = true;
+        }
+
+        int stringStart = new Random().nextInt(text.length() / 2);
+
+        System.out.println(text.substring(stringStart, stringStart + (int)Math.sqrt(text.length())));
+        System.out.println("Понятин ли вам этот текст? Y/N");
+
+        Scanner scanner = new Scanner(System.in);
+        String nextLine = scanner.nextLine();
+        if (nextLine.equals("Y") || nextLine.equals("y")) {
+            flag = true;
+        } else if (nextLine.equals("N") || nextLine.equals("n")){
+            return false;
+        }
+        return flag;
+    }
 }
