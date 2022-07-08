7dfa8f0ba3f2f53a16d198c86c9e1ead8008e3ea
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2022-07-08 14:57:41 +0300

    rename class file

--- src/Menu.java
+++ src/Menu.java
@@ -2,7 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Main {
+public class Menu {
 
     public static void main(String[] args) throws IOException {
 
