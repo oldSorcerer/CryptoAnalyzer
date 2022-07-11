ea45f03e9ab47bc355f2f267e704cee6349f9bd1
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2022-07-11 12:36:55 +0300

    del zero class

--- src/Menu.java
+++ src/Menu.java
@@ -5,12 +5,10 @@ import java.io.InputStreamReader;
 public class Menu {
 
     public static void main(String[] args) throws IOException {
-
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
         while (true) {
             System.out.println("\n" + "Выберете действие, введя его номер: \n" +
-                    "0. Воспользоваться Терморектальным криптоанализатором. \n" +
                     "1. Зашифровать текст в файле с помощью ключа. \n" +
                     "2. Расшифровать текст в файле с помощью ключа. \n" +
                     "3. Подобрать ключ к зашифрованому тексу в файле (brute force). \n" +
@@ -20,11 +18,10 @@ public class Menu {
             String stringSwitch = reader.readLine();
 
             switch (stringSwitch) {
-                case ("0") -> ChoiceZero.choiceZero();
-                case ("1") -> ChoiceOne.choiceOne();
-                case ("2") -> ChoiceTwo.choiceTwo();
-                case ("3") -> ChoiceThree.choiceThree();
-                case ("4") -> ChoiceFour.choiceFour();
+                case ("1") -> new ChoiceOne().choiceOne();
+                case ("2") -> new ChoiceTwo().choiceTwo();
+                case ("3") -> new ChoiceThree().choiceThree();
+                case ("4") -> new ChoiceFour().choiceFour();
             }
             if (stringSwitch.equals("exit")) {
                 break;
