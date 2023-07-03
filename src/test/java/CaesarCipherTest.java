f880fa6582f22702e759c6edfd57c6d646f15974
oldSorcerer <aleksei.derevyannykh@gmail.com>
2023-07-03 21:23:27 +0300

    add new test

--- src/test/java/CaesarCipherTest.java
+++ src/test/java/CaesarCipherTest.java
@@ -19,4 +19,39 @@ public class CaesarCipherTest {
         assertEquals(caesar.decrypt(stringEncrypt, key), stringNotEncrypt);
     }
 
+    @Test
+    public void encrypt1() {
+        assertEquals(caesar.encrypt("abc", 3),"def");
+    }
+
+    @Test
+    public void encrypt2() {
+        assertEquals(caesar.encrypt("abc", 29),"def");
+    }
+
+    @Test
+    public void encrypt3() {
+        assertEquals(caesar.encrypt("zoo", 3),"crr");
+    }
+
+    @Test
+    public void encrypt4() {
+        assertEquals(caesar.encrypt("Δ", 3),"");
+    }
+
+    @Test
+    public void decrypt1() {
+        assertEquals(caesar.decrypt("def", 3), "abc");
+    }
+
+    @Test
+    public void decrypt2() {
+        assertEquals(caesar.decrypt("def", 29),"abc");
+    }
+
+    @Test
+    public void decrypt3() {
+        assertEquals(caesar.decrypt("crr", 3),"zoo");
+    }
+
 }
