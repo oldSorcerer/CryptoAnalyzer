c2da8da5b20cb95d3428c7b56d7f327e07d6c978
oldSorcerer <aleksei.derevyannykh@gmail.com>
2024-09-03 14:20:11 +0300

    add UtilityClass annotation

--- src/main/java/ConsoleHelper.java
+++ src/main/java/ConsoleHelper.java
@@ -1,29 +1,28 @@
-import lombok.AccessLevel;
-import lombok.NoArgsConstructor;
+import lombok.experimental.UtilityClass;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.file.Path;
 
-@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@UtilityClass
 public class ConsoleHelper {
 
     private static final BufferedReader CONSOLE = new BufferedReader(new InputStreamReader(System.in));
 
-    public static void writeMessage(String message) {
+    public void writeMessage(String message) {
         System.out.println(message);
     }
 
-    public static String readString() throws IOException {
+    public String readString() throws IOException {
         return CONSOLE.readLine();
     }
 
-    public static int readInt() throws IOException {
+    public int readInt() throws IOException {
         return Integer.parseInt(readString().trim());
     }
 
-    public static Path buildFileName(String path, String suffix) {
+    public Path buildFileName(String path, String suffix) {
         Path fullPath = Path.of(path);
         Path parent = fullPath.getParent();
         String fileName = fullPath.getFileName().toString();
