12d4e25a5485eecab1e0be03c5dbe4e8f072d94a
oldSorcerer <aleksei.derevyannykh@gmail.com>
2024-09-03 13:47:27 +0300

    refactoring code using stream

--- src/main/java/Parsing.java
+++ src/main/java/Parsing.java
@@ -2,11 +2,13 @@ import lombok.SneakyThrows;
 
 import java.nio.file.*;
 import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class Parsing {
 
     @SneakyThrows
-    public void parse()  {
+    public void parse() {
 
         ConsoleHelper.writeMessage("Введите полный путь к файлу, для его расшифровки:");
         String src = ConsoleHelper.readString();
@@ -16,22 +18,30 @@ public class Parsing {
 
         Path dst = ConsoleHelper.buildFileName(src, "_parsing");
 
-        List<Map.Entry<Character, Integer>> listEncrypted = convertToList(src);
-        List<Map.Entry<Character, Integer>> listStatistic = convertToList(pathStatistic);
+        List<Map.Entry<String, Long>> listEncrypted = convertToList(src);
+        List<Map.Entry<String, Long>> listStatistic = convertToList(pathStatistic);
 
         if (listEncrypted.size() <= listStatistic.size()) {
 
-            Map<Character, Character> decrypted = new HashMap<>();
-            for (int i = 0; i < listEncrypted.size(); i++) {
-                decrypted.put(listEncrypted.get(i).getKey(), listStatistic.get(i).getKey());
-            }
+            Map<String, String> decrypted = IntStream.range(0, listEncrypted.size())
+                    .boxed()
+                    .collect(Collectors.toMap(i -> listEncrypted.get(i).getKey(), i -> listStatistic.get(i).getKey()));
 
-            String content = Files.readString(Path.of(src));
-            StringBuilder builder = new StringBuilder();
-            for (char encryptedChar : content.toCharArray()) {
-                builder.append(decrypted.get(encryptedChar));
-            }
-            Files.writeString(dst, builder);
+            Files.writeString(dst, Arrays.stream(Files.readString(Path.of(src)).split(""))
+                    .map(decrypted::get)
+                    .collect(Collectors.joining()));
+
+//            Map<Character, Character> decrypted = new HashMap<>();
+//            for (int i = 0; i < listEncrypted.size(); i++) {
+//                decrypted.put(listEncrypted.get(i).getKey(), listStatistic.get(i).getKey());
+//            }
+//
+//            String content = Files.readString(Path.of(src));
+//            StringBuilder builder = new StringBuilder();
+//            for (char encryptedChar : content.toCharArray()) {
+//                builder.append(decrypted.get(encryptedChar));
+//            }
+//            Files.writeString(dst, builder);
 
             ConsoleHelper.writeMessage("Содержимое файла расшифровано методом статистического анализа." + System.lineSeparator());
 
@@ -41,18 +51,18 @@ public class Parsing {
     }
 
     @SneakyThrows
-    private List<Map.Entry<Character, Integer>> convertToList(String path) {
+    private List<Map.Entry<String, Long>> convertToList(String path) {
 
-        Map<Character, Integer> map = new HashMap<>();
-
-        for (char aChar : Files.readString(Path.of(path)).toCharArray()) {
-            map.merge(aChar, 1, Integer::sum);
-        }
-        return map.entrySet().stream().sorted(Map.Entry.<Character, Integer>comparingByValue().reversed()).toList();
-        /*
-        *  return Arrays.stream(Files.readString(Path.of(path)).split(""))
+        return Arrays.stream(Files.readString(Path.of(path)).split(""))
                 .collect(Collectors.groupingBy(str -> str, Collectors.counting())).entrySet().stream()
-                .sorted(Map.Entry.<String, Long>comparingByValue().reversed()).toList();
-        * */
+                .sorted(Map.Entry.<String, Long>comparingByValue().reversed())
+                .toList();
+
+//        Map<Character, Integer> map = new HashMap<>();
+//
+//        for (char aChar : Files.readString(Path.of(path)).toCharArray()) {
+//            map.merge(aChar, 1, Integer::sum);
+//        }
+//        return map.entrySet().stream().sorted(Map.Entry.<Character, Integer>comparingByValue().reversed()).toList();
     }
 }
